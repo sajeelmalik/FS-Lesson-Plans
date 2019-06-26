94ee58c36a3b16f24e3b7fadb7a247caaed9867e
Sajeel Malik <sajeel23@berkeley.edu>
2019-06-26 13:34:40 -0700

    write npm package creation exercises

---
+++ npm/03-MyGoogle/Solved/google.js
@@ -0,0 +1,63 @@
+// Once you've created your package.json, we can start writing our code for our search engine!
+
+var opn = require("opn");
+
+// Create a constructor function named "Google" (ideally, we would want to create a Class, but let's stick to ES5 Javascript for now). 
+
+var Google = function(){
+
+    // Let's create a basic search function named "search" (this.search) that takes in two arguments, an array and a variable.
+    this.search = function(arr, x){
+
+        // Create a results object with two keys: positions (an empty array), and count (a number starting at 0).
+        var results = {
+            positions: [],
+            count: 0,
+            time: 0 // BONUS #2
+        };
+
+        
+        var start = process.hrtime();
+
+        // Search (hint: iterate) through the array and find all the instances of the variable. 
+        for(var i = 0; i < arr.length; i++){
+            // When you find the variable in the array, update the results object; push the current index to our "positions" array, and increment the "count" by one.
+            if(arr[i] === x){
+                results.positions.push(i);
+                results.count++;
+            }
+        }
+
+        // BONUS #2
+        results.time = (parseFloat(process.hrtime(start)[0]) + parseFloat("." + process.hrtime(start)[1])).toFixed(2);
+
+        // The function should return our results object.
+        return results;
+    },
+
+    // BONUS #1
+    this.searchOnline = function(str){
+        opn(`https://www.google.com/search?q=${str}`);
+    }
+}
+
+// Export your Google constructor
+
+module.exports = Google;
+
+// BONUSES: 
+
+// 1. Create a second function called "searchOnline" (this.searchOnline) which takes in a string as a search query and opens a browser search for that query. Remember, we are running in nodejs.
+
+// 2. In your results object, add a key for "time" that RETURNS the time it took, in ms, for your search execution to run. This may be hard to find.
+
+
+// Tests (uncomment these to test):
+
+var google = new Google;
+
+// console.log(google.search([1, 2, 3, 4, 5, 6, 2, 9, 10, 11, 2], 2));
+
+console.log(google.search(["captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch","captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch"], "captain america"));
+
+// google.searchOnline("James Harden Beard");
