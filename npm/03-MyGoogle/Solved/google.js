a5ce2366a841d8bb202c58266f0e60a5f5c789ec
Sajeel Malik <sajeel23@berkeley.edu>
2019-06-27 11:31:48 -0700

    remove all constructor references

--- npm/03-MyGoogle/Solved/google.js
+++ npm/03-MyGoogle/Solved/google.js
@@ -1,6 +1,6 @@
 // Once you've created your package.json, we can start writing our code for our search engine!
 
-var opn = require("opn"); // BONUS #1
+// var opn = require("opn"); // BONUS #1
 
 // Create a object named "Google" (ideally, we would want to create a Class, but let's stick to ES5 Javascript for now). 
 
@@ -54,7 +54,7 @@ module.exports = Google;
 
 // Tests (uncomment these individually to test):
 
-var google = new Google;
+var google = Google;
 
 // console.log(google.search([1, 2, 3, 4, 5, 6, 2, 9, 10, 11, 2], 2));
 // { positions: [ 1, 6, 10 ], count: 3, time: '0.30' }
@@ -62,5 +62,5 @@ var google = new Google;
 console.log(google.search(["captain america", "thor", "black widow", "captain america", "captain america", "iron man", "quicksilver", "spiderman", "starlord", "captain america", "ant man", "dr. strange", "thanos", "captain marvel", "gamora", "hulk", "nebula", "hawkeye", "vision", "scarlet witch"], "captain america"));
 // { positions: [ 0, 3, 4, 9 ], count: 4, time: '0.13' }
 
-google.searchOnline("James Harden Beard");
+// google.searchOnline("James Harden Beard");
 // Should open a browser search.
